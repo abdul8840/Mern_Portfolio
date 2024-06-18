f284d912b779df3472f08bf4cfea03caa44c630d
Abdul-Rahman <abdul14941naseer@gmail.com>
2024-06-18 17:52:09 +0530

    add signup page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,14 +1,53 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { FaGithub, FaLinkedin,FaDiscord } from "react-icons/fa";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false)
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.name || !formData.username || !formData.email || !formData.password) {
+      setError("Please fill in all fields");
+    }
+    try {
+      setLoading(true);
+      setError(null);
+      const res = await fetch('api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success === false){
+        return setError(data.message);
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  }
+
+  console.log(formData)
   return (
     <div className="min-h-screen mt-20">
       <div className="flex gap-5 p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center">
         {/* leftside */}
         <div className="flex-1">
-          <span className="text-xl text-gray-500 block mb-1">Hello,</span>
+          <span className="text-xl text-gray-500 block mb-1">Hello, I Am</span>
           <Link to="/" className="font-bold dark:text-white text-4xl">
             Abdul Rahman
           </Link>
@@ -16,17 +55,23 @@ const SignUp = () => {
             I'm a creative web designer based in Maharastra India, and i'm very
             passionate and dedicated to my work.
           </p>
+          <div className="mt-8 flex justify-start text-3xl gap-6 dark:text-white">
+          <Link to=''><FaGithub /></Link>
+          <Link to=''><FaLinkedin /></Link>
+          <Link to=''><FaDiscord /></Link>
+          </div>
         </div>
         {/* rightside */}
         <div className="flex-1">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div>
               <Label value="Your Name" />
               <TextInput
                 type="text"
                 placeholder="Your Name"
                 id="name"
-                required={true}
+                // required={true}
+                onChange={handleChange}
               />
             </div>
             <div className="mt-2">
@@ -35,7 +80,8 @@ const SignUp = () => {
                 type="text"
                 placeholder="abdul001"
                 id="username"
-                required={true}
+                // required={true}
+                onChange={handleChange}
               />
             </div>
             <div className="mt-2">
@@ -44,7 +90,8 @@ const SignUp = () => {
                 type="email"
                 placeholder="abdul123@portfolio.com"
                 id="email"
-                required={true}
+                // required={true}
+                onChange={handleChange}
               />
             </div>
             <div className="mt-2">
@@ -53,23 +100,36 @@ const SignUp = () => {
                 type="password"
                 placeholder="********"
                 id="password"
-                required={true}
+                // required={true}
+                onChange={handleChange}
               />
             </div>
             <Button
               className="w-full mt-5"
               gradientDuoTone="pinkToOrange"
               type="submit"
+              disabled={loading}
             >
-              Sign Up
+              {loading ? (
+                <>
+                <Spinner size='sm' />
+                <span className="pl-3">Loading...</span>
+                </>
+              ) : "Sign Up"}
             </Button>
           </form>
+          
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account? </span>
             <Link to="/sign-in" className="text-blue-500">
               Sign In
             </Link>
           </div>
+          {
+            error && (
+              <Alert className="mt-5" color='failure'>{error}</Alert>
+            )
+          }
         </div>
       </div>
     </div>
