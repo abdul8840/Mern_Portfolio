9dc3f7677ec0bc385d9ec833bcd40542076c29c7
Abdul-Rahman <abdul14941naseer@gmail.com>
2024-06-18 14:57:43 +0530

    create signup page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,10 +8,11 @@ const Header = () => {
 
   return (
     <div className="border-b-2">
-      <Navbar className='max-w-[1000px] mx-auto'>
+      <Navbar className='max-w-[1000px] mx-auto flex justify-between items-center'>
         <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
           Abdul
         </Link>
+        
         <div className="flex gap-2 md:order-2">
           <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
             <FaMoon />
@@ -26,14 +27,13 @@ const Header = () => {
           <Navbar.Toggle />
         </div>
 
-        <Navbar.Collapse className="text-black md:flex md:items-center">
-          <Navbar.Link className={path === '/' ? 'text-blue-500' : 'text-black'} as={'div'}>
-            <Link to='/' className='block py-2 pr-4 pl-3 md:p-0'>Home</Link>
+        <Navbar.Collapse className="md:max-w-20 text-black md:flex md:items-center">
+          <Navbar.Link active={path === '/'} as='div'>
+            <Link to='/' className={path === '/' ? 'text-blue-500 block py-2 pr-4 pl-3 md:p-0' : 'text-black block py-2 pr-4 pl-3 md:p-0'}>Home</Link>
           </Navbar.Link>
-          <Navbar.Link className={path === '/about' ? 'text-blue-500' : 'text-black'} as={'div'}>
-            <Link to='/about' className='block py-2 pr-4 pl-3 md:p-0'>About</Link>
+          <Navbar.Link active={path === '/about'} as='div'>
+            <Link to='/about' className={path === '/about' ? 'text-blue-500 block py-2 pr-4 pl-3 md:p-0' : 'text-black block py-2 pr-4 pl-3 md:p-0'}>About</Link>
           </Navbar.Link>
-          
         </Navbar.Collapse>
       </Navbar>
     </div>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import { Button, Label, TextInput } from "flowbite-react";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="min-h-screen mt-20">
+      <div className="flex gap-5 p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center">
+        {/* leftside */}
+        <div className="flex-1">
+          <span className="text-xl text-gray-500 block mb-1">Hello,</span>
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            Abdul Rahman
+          </Link>
+          <p className="text-sm mt-5 font-[500] text-gray-500">
+            I'm a creative web designer based in Maharastra India, and i'm very
+            passionate and dedicated to my work.
+          </p>
+        </div>
+        {/* rightside */}
+        <div className="flex-1">
+          <form>
+            <div>
+              <Label value="Your Name" />
+              <TextInput
+                type="text"
+                placeholder="Your Name"
+                id="name"
+                required={true}
+              />
+            </div>
+            <div className="mt-2">
+              <Label value="Abdul" />
+              <TextInput
+                type="text"
+                placeholder="abdul001"
+                id="username"
+                required={true}
+              />
+            </div>
+            <div className="mt-2">
+              <Label value="Your Email" />
+              <TextInput
+                type="email"
+                placeholder="abdul123@portfolio.com"
+                id="email"
+                required={true}
+              />
+            </div>
+            <div className="mt-2">
+              <Label value="Your Password" />
+              <TextInput
+                type="password"
+                placeholder="********"
+                id="password"
+                required={true}
+              />
+            </div>
+            <Button
+              className="w-full mt-5"
+              gradientDuoTone="pinkToOrange"
+              type="submit"
+            >
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account? </span>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
