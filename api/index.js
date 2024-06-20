31a244a58cf45b8c12e665acaf412cf13fac71bf
Abdul-Rahman <abdul14941naseer@gmail.com>
2024-06-20 21:05:09 +0530

    complete update profile page and delete user functionality

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(PORT, () => {
-  console.log('Server is running on port 8000!');
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.use('/api/user', userRoutes);
