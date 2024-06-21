88e7c3e54f37f03f4335ab9ef08805e95fd5b7a9
Abdul-Rahman <abdul14941naseer@gmail.com>
2024-06-21 15:24:23 +0530

    show posts in admin dashboard

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -31,28 +31,32 @@ export const getposts = async (req, res, next) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
-    const sortDirection = req.query.order === 'asc' ?  1 : -1;
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
     const posts = await Post.find({
-      ...Post(req.query.userId && { userId: req.query.userId }),
-      ...Post(req.query.category && { category: req.query.category }),
-      ...Post(req.query.description && { description: req.query.description }),
-      ...Post(req.query.slug && { slug: req.query.slug }),
-      ...Post(req.query.postId && { _id: req.query.postId }),
-      ...Post(req.query.searchTerm && {
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.description && { description: req.query.description }),
+      ...(req.query.slug && { category: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
         $or: [
-          { title: { $regex: req.query.searchTerm, $options: 'i' }},
-          { description: { $regex: req.query.searchTerm, $options: 'i' }},
-          { content: { $regex: req.query.searchTerm, $options: 'i' }}
+          { title: { $regex: req.query.searchTerm, $options: 'i' } },
+          { content: { $regex: req.query.searchTerm, $options: 'i' } },
         ],
       }),
-    }).sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit);
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
 
     const totalPosts = await Post.countDocuments();
+
+    const now = new Date();
 
     const oneMonthAgo = new Date(
       now.getFullYear(),
       now.getMonth() - 1,
-      now.getDate(),
+      now.getDate()
     );
 
     const lastMonthPosts = await Post.countDocuments({
@@ -62,10 +66,9 @@ export const getposts = async (req, res, next) => {
     res.status(200).json({
       posts,
       totalPosts,
-      lastMonthPosts
+      lastMonthPosts,
     });
-
   } catch (error) {
-    
+    next(error);
   }
-}
+};
