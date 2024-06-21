0c6e2b3c55a047bd846dcfc74a85f3b78e820f9b
Abdul-Rahman <abdul14941naseer@gmail.com>
2024-06-21 19:11:52 +0530

    add update post functionality in admin dashboard

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,4 +1,3 @@
-import { now } from 'mongoose';
 import Post from '../models/post.model.js';
 import { errorHandler } from '../utils/error.js';
 
@@ -6,7 +5,7 @@ export const create = async (req, res, next) => {
   if (!req.user.isAdmin) {
     return next(errorHandler(403, 'You are not allowed to create a post'));
   }
-  if (!req.body.title || !req.body.content || !req.body.description) {
+  if (!req.body.title || !req.body.content) {
     return next(errorHandler(400, 'Please provide all required fields'));
   }
   const slug = req.body.title
@@ -35,8 +34,7 @@ export const getposts = async (req, res, next) => {
     const posts = await Post.find({
       ...(req.query.userId && { userId: req.query.userId }),
       ...(req.query.category && { category: req.query.category }),
-      ...(req.query.description && { description: req.query.description }),
-      ...(req.query.slug && { category: req.query.slug }),
+      ...(req.query.slug && { slug: req.query.slug }),
       ...(req.query.postId && { _id: req.query.postId }),
       ...(req.query.searchTerm && {
         $or: [
@@ -74,15 +72,36 @@ export const getposts = async (req, res, next) => {
 };
 
 export const deletepost = async (req, res, next) => {
-  if(!req.user.isAdmin || req.user.id !== req.params.userId) {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
     return next(errorHandler(403, 'You are not allowed to delete this post'));
   }
   try {
     await Post.findByIdAndDelete(req.params.postId);
-    res.status(200).json('Post deleted successfully');
-    
+    res.status(200).json('The post has been deleted');
   } catch (error) {
     next(error);
   }
-}
+};
 
+export const updatepost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to update this post'));
+  }
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.postId,
+      {
+        $set: {
+          title: req.body.title,
+          content: req.body.content,
+          category: req.body.category,
+          image: req.body.image,
+        },
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    next(error);
+  }
+};
