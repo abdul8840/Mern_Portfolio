c7362c38fbba97ad4253506185b4785ab573bfa8
Abdul-Rahman <abdul14941naseer@gmail.com>
2024-06-22 17:19:41 +0530

    add functionality to delete skill

--- api/routes/skill.route.js
+++ api/routes/skill.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { verifyToken } from '../utils/verifyUser.js';
-import { createskill, getskills } from '../controllers/skill.controller.js';
+import { createskill, getskills, deleteskill } from '../controllers/skill.controller.js';
 
 const router = express.Router();
 
 router.post('/createskill', verifyToken, createskill);
 router.get('/getskills', getskills);
+router.delete('/deleteskill/:postId/:userId', verifyToken, deleteskill)
 
 export default router;
--- client/src/components/DashSkills.jsx
+++ client/src/components/DashSkills.jsx
@@ -3,11 +3,14 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { Modal, Table } from 'flowbite-react';
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 const DashSkills = () => {
   const {currentUser} = useSelector((state) => state.user);
   const [userSkills, setUserSkills] = useState([]);
   const [showMore, setShowMore] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [skillIdToDelete, setSkillIdToDelete] = useState('')
 
   console.log(userSkills)
 
@@ -47,11 +50,35 @@ const DashSkills = () => {
       }
 
   }
+
+  const handleDeleteSkill = async () => {
+    setShowModal(false);
+    try {
+      const res = await fetch(
+        `/api/skill/deleteSkill/${skillIdToDelete}/${currentUser._id}`,
+        {
+          method: 'DELETE',
+        }
+      );
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        setUserSkills((prev) =>
+          prev.filter((skill) => skill._id !== skillIdToDelete)
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+      <div className="w-full flex justify-end mt-2 mb-2">
       <Link to="/create-skill">
         <Button gradientDuoTone="purpleToPink">Add Skill</Button>
       </Link>
+      </div>
 
       {currentUser.isAdmin && userSkills.length > 0 ? (
         <>
@@ -61,10 +88,8 @@ const DashSkills = () => {
               <Table.HeadCell>Skill image</Table.HeadCell>
               <Table.HeadCell>Technology</Table.HeadCell>
               <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Level</Table.HeadCell>
               <Table.HeadCell>Delete</Table.HeadCell>
-              <Table.HeadCell>
-                <span>Edit</span>
-              </Table.HeadCell>
             </Table.Head>
             {userSkills.map((skill) => (
               <Table.Body className='divide-y'>
@@ -90,24 +115,17 @@ const DashSkills = () => {
                     </Link>
                   </Table.Cell>
                   <Table.Cell>{skill.category}</Table.Cell>
+                  <Table.Cell>{skill.percent}%</Table.Cell>
                   <Table.Cell>
                   <span
-                      // onClick={() => {
-                      //   setShowModal(true);
-                      //   setPostIdToDelete(post._id);
-                      // }}
+                      onClick={() => {
+                        setShowModal(true);
+                        setSkillIdToDelete(skill._id);
+                      }}
                       className='font-medium text-red-500 hover:underline cursor-pointer'
                     >
                       Delete
                     </span>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Link
-                      className='text-teal-500 hover:underline'
-                      to={`/update-skill/${skill._id}`}
-                    >
-                      <span>Edit</span>
-                    </Link>
                   </Table.Cell>
                 </Table.Row>
               </Table.Body>
@@ -125,7 +143,30 @@ const DashSkills = () => {
         <p>You have no skills yet!</p>
       )}
       
-          
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        popup
+        size="md"
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this skill
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button color="failure" onClick={handleDeleteSkill}>
+                Yes, I'm sure
+              </Button>
+              <Button color="gray" onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
       
       
     </div>
